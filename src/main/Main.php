9315e84871c8ee4cf931b43bef6b841c832d8e6e
NycuRO <nghergu@yahoo.com>
2016-10-17 19:36:41 +0300

    Update Main.php

--- src/main/Main.php
+++ src/main/Main.php
@@ -5,7 +5,6 @@ namespace main;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
 use pocketmine\utils\TextFormat as C;
 use synapse\Player;
 
@@ -20,10 +19,9 @@ class Main extends PluginBase {
   public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
     switch($cmd->getName()){
       case "hub":
-        if ($sender instanceof Player) {
-           $player->transfer(HUB);
+        $player->transfer(HUB);
         } else {
-          $sender->sendMessage(C::RED."Use this command in-game!");
+          $sender->sendMessage(C::RED."Error!");
         break;
         }
     }
