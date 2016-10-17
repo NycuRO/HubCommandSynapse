38d1e8ee1edada9d81f35dcdad783958116be0d3
NycuRO <nghergu@yahoo.com>
2016-10-17 19:41:37 +0300

    Update Main.php

--- src/main/Main.php
+++ src/main/Main.php
@@ -8,20 +8,21 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as C;
 use synapse\Player;
 
-class Main extends PluginBase {
+class Main extends PluginBase{
+  
   public function onEnable(){
-    $this->getLogger()->info(C::GREEN."Plugin HubCommandSynapse enables!");
+    $this->getLogger()->info(C::GREEN."Plugin HubCommandSynapse enabled!");
   }
-  public function onDisable() {
-    $this->getLogger()->info(C::RED."Plugin HubCommandSynapse disenables!");
+  
+  public function onDisable(){
+    $this->getLogger()->info(C::RED."Plugin HubCommandSynapse disabled!");
   }
+  
   public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
-    switch($cmd->getName()) {
+    switch($cmd->getName()){
       case "hub":
         $player->transfer(HUB);
         break;
-        return true;
-        }
     }
   }
 }
